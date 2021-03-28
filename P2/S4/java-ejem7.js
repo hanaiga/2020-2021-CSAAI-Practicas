6bfd79c1296c5fa1a56b7d44afff0b6ec0867b26
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-28 22:05:42 +0200

    ejemplo 7 java

--- P2/S4/java-ejem7.js
+++ P2/S4/java-ejem7.js
@@ -1,7 +1,8 @@
 // metemos la funcion directamente en en el click
+console.log('Ejecutando js ...')
 
 const prueba = document.getElementById('prueba')
 
-prueba.onclick = function(){
+prueba.onclick = function() {
     console.log('se ha hecho click sobre el parrafo')
 }
