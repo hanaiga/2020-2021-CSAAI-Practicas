d0707d3e0566f0941c9a314cec0758c8c122e9ea
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-28 21:35:44 +0200

    ejemplo 5 java

---
+++ P2/S4/java-ejem5.js
@@ -0,0 +1,7 @@
+// acceder a elementos del HTML
+const test = document.getElementById('prueba')
+
+// ahora para mostrar en la consola lo q ha leido el js
+
+console.log(' lo que ha leido el js es:')
+console.log(test.innerHTML)
