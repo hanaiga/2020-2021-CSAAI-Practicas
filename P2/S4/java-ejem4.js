b9df2a6e5252220d6405a43d0d7785008845e019
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-28 21:14:01 +0200

    ejemplo 4

---
+++ P2/S4/java-ejem4.js
@@ -0,0 +1,5 @@
+// incluimos lo de defer en el html para garantizar que el codigo js
+// empiece a ejecutar una vez el html ha sido procesado completamente
+
+console.log('imprimiendo mensaje');
+console.log('incluido el defer....');
