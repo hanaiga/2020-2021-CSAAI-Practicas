c400cb17e4b2dab344350d2137510f061fb54805
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-31 01:39:26 +0200

    ejercicio 1 L6

---
+++ P2/L6/ejer1.js
@@ -0,0 +1,29 @@
+
+
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display");
+boton1 = document.getElementById("boton1");
+boton2 = document.getElementById("boton2");
+suma = document.getElementById("suma");
+igual = document.getElementById("igual");
+clear = document.getElementById("clear");
+
+boton1.onclick = () =>{
+    display.innerHTML += "1";
+}
+boton2.onclick = () =>{
+    display.innerHTML += "2";
+}
+
+suma.onclick = () =>{
+    display.innerHTML += "+";
+}
+
+igual.onclick = () =>{
+    display.innerHTML = eval(display.innerHTML);
+}
+
+boton1.onclick = () =>{
+    display.innerHTML = "0";
+}
