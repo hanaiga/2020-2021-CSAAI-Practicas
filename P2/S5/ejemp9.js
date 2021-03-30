cf96ba20a998a687f74151ac4a17a2e85789f712
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-31 01:22:34 +0200

    ejemplo9

---
+++ P2/S5/ejemp9.js
@@ -0,0 +1,18 @@
+console.log("Ejecutando JS...");
+
+const botones = document.getElementsByClassName("digito");
+
+function digito(Value){
+
+    console.log("valor: " + value)
+}
+
+for (let boton of botones) {
+
+    //-- Establecer la funcion de llamada del boton i
+    //-- El parámetro ev.target contiene el boton
+    //-- que ha recibido el click
+    boton.onclick = (ev) => {
+        digito(ev.target.value)
+    }
+}
