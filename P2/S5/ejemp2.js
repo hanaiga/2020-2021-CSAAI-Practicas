bdc0b12c565ba306edd9dbe356d1a2ee44cce415
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-03-30 02:24:14 +0200

    ejemplo2

---
+++ P2/S5/ejemp2.js
@@ -0,0 +1,20 @@
+console.log("Ejecutando JS...");
+
+const display = document.getElementById("display");
+
+const boton = document.getElementById("boton");
+
+// inicializar contador
+
+let cont = 0;
+
+// funcion para que vaya contando cada vez q se toque boton
+
+boton.onclick = () => {
+
+    console.log("click!");
+
+    cont += 1;
+
+    display.innerHTML += ' ' + cont;
+}
