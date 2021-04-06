0d434122c525729f529c04f066426f760671e4f5
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-04-06 23:46:56 +0200

    cambios

---
+++ P2/calculadora.js
@@ -0,0 +1,15 @@
+console.log("Ejecutando JS...");
+
+display = document.getElementById("display");
+igual = document.getElementById("igual");
+reset = document.getElementById("reset");
+delet = document.getElementById("delet");
+coma = document.getElementById("coma");
+
+// ahora recojer un array con todos los digitos
+
+let botones = document.getElementsByClassName("boton");
+
+// Ahora un array con todas las operaciones
+
+let oper = document.getElementsByClassName("oper");
