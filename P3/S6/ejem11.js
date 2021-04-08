5c02dfaa133437f9fa424b1cc7c8778f2a52c968
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-04-08 10:52:17 +0200

    ejem 11 2d

---
+++ P3/S6/ejem11.js
@@ -0,0 +1,54 @@
+console.log("Ejecutando Js...");
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = 400;
+canvas.height = 200;
+
+const ctx = canvas.getContext("2d");
+
+// indico la posicion del objeto que vamos a animar
+
+let x = 0;
+let y = 10;
+
+// defino la velicidad en direccion x
+let velx = 1;
+
+// esta es la funcion principal para animar
+
+function update(){
+
+    console.log("test");
+    // ahora quiero que se mueva entonces defino el movimiento
+    // para que no se salga del canvas y rebote
+    // le resto el tamaño del rectangulo para que no se vea como el rec sale
+    if (x <0 || x >= canvas.width - 20 ){
+        velx = -velx;
+    }
+    x = x + velx;
+    // borro canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // dibujo los elementos
+    ctx.beginPath();
+
+    // le digo que dibuje un rectangulo en la coordenada 5,5 y de 100 *50
+        ctx.rect(x,y, 20,20);
+
+        // le digo el color de relleno
+        ctx.fillStyle = 'red';
+
+        // aplico el relleno
+        ctx.fill();
+
+        // muestro el contorno del rectangulo
+        ctx.stroke();
+        
+    ctx.closePath();
+    // que vuelva a ejecutar update cuando le toque
+    requestAnimationFrame(update);
+}
+
+// llamo a la funion para que empieze a rular
+update();
