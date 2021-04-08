f1291c37f59e0d2d954ceb96574fb805c8621f4f
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-04-08 09:38:54 +0200

    ejem 3 anim  2D

---
+++ P3/S6/ejem3.js
@@ -0,0 +1,31 @@
+console.log("Ejecutando Js...");
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = 400;
+canvas.height = 200;
+
+// para obtener el contexto, osea 2D o 3D
+
+const ctx = canvas.getContext("2d");
+
+// al dibujar el objeto lo delimitaremos por begin path y close path
+
+ctx.beginPath();
+
+    // le digo que dibuje un rectangulo en la coordenada 5,5 y de 100 *50
+    ctx.rect(5,5, 100,50);
+
+        // le digo el color de relleno
+        ctx.fillStyle = 'red';
+
+        // quiero q el borde sea mas grueso
+        ctx.lineWidth = 4;
+
+        // aplico el relleno
+        ctx.fill();
+
+        // muestro el contorno del rectangulo
+        ctx.stroke();
+        
+ctx.closePath();
