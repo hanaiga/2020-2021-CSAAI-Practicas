1a8a3f20d91e2d87d3dbb8d09ea0203d3cb85073
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-05-07 02:10:18 +0200

    filtro componentes RGB terminado

--- P4/L11/ejercicio2.js
+++ P4/L11/ejercicio2.js
@@ -4,12 +4,16 @@ console.log("Ejecutando JS....")
 const canvas = document.getElementById('canvas');
 const imgen = document.getElementById('foto');
 const ctx = canvas.getContext('2d');
+
 const deslizador_R = document.getElementById('deslizador_R');
 const deslizador_G = document.getElementById('deslizador_G');
 const deslizador_B = document.getElementById('deslizador_B');
-const rango = document.getElementById('rango');
+const rango_R = document.getElementById('rango_R');
+const rango_G = document.getElementById('rango_G');
+const rango_B = document.getElementById('rango_B');
 
 // funcion que nos devuelve la imagen una vez cargada 100%
+
 imgen.onload = function () {
 
   // establecemos el canvas del tamaño de la imagen
@@ -21,9 +25,12 @@ imgen.onload = function () {
   console.log("imagen Cargada con éxito");
 };
 
-// establecemos funcion retrollamada del deslizador
+
+// establecemos funcion retrollamada del deslizador rojo
 deslizador_R.oninput = () => {
 
+    rango_R.innerHTML = deslizador_R.value;
+
     //situamos la imagen original en el canvas
     ctx.drawImage(imgen, 0,0);
 
@@ -33,27 +40,32 @@ deslizador_R.oninput = () => {
     // Obtenemos un array con todos los píxeles de la imagen
     let data = imgData.data
 
-    // obtenemos umbral del deslizador
-    umbral = deslizador_R.value;
+    // obtenemos umbral de los deslizadores
+    let umbral_R = deslizador_R.value;
+    let umbral_G = deslizador_G.value;
+    let umbral_B = deslizador_B.value;
 
-    // ahora borramos la componente roja de la imagen
+    // vamos borrando componentes segun umbrales
     for(let i= 0; i<data.length; i+=4){
-        if (data[i] > umbral)
-         data[i] = umbral;
-      
-        // si queremos quitar la roja y la verde y quedarnos solo con azul
-        // activamos ambas data[i] y data[i+1] 
-        // si queremos quitar la verde es data[i+1]
-        // si queremos quitar la azul es data[i+2]
+        if (data[i] > umbral_R){
+            data[i] = umbral_R;
+        }
+        if (data[i+1] > umbral_G){
+            data[i+1] = umbral_G;
+        }
+        if (data[i+2] > umbral_B){
+            data[i+2] = umbral_B;
+        }    
     }
-
-    // actualizamos la imagen para que salga el pixel pintado
+    // actualizamos la imagen 
     ctx.putImageData(imgData, 0, 0);
 
-
 };
+
+// funcion retrollamada del deslizador verde
 deslizador_G.oninput = () => {
 
+    rango_G.innerHTML = deslizador_G.value;
     //situamos la imagen original en el canvas
     ctx.drawImage(imgen, 0,0);
 
@@ -64,26 +76,34 @@ deslizador_G.oninput = () => {
     let data = imgData.data
 
     // obtenemos umbral del deslizador
-    umbral = deslizador_G.value;
+    let umbral_R = deslizador_R.value;
+    let umbral_G = deslizador_G.value;
+    let umbral_B = deslizador_B.value;
 
     // ahora borramos la componente roja de la imagen
     for(let i= 0; i<data.length; i+=4){
-        if (data[i] > umbral)
-         data[i+1] = umbral;
-      
-        // si queremos quitar la roja y la verde y quedarnos solo con azul
-        // activamos ambas data[i] y data[i+1] 
-        // si queremos quitar la verde es data[i+1]
-        // si queremos quitar la azul es data[i+2]
+        if (data[i] > umbral_R){
+            data[i] = umbral_R;
+        }
+        if (data[i+1] > umbral_G){
+            data[i+1] = umbral_G;
+        }
+        if (data[i+2] > umbral_B){
+            data[i+2] = umbral_B;
+        }    
     }
+
 
     // actualizamos la imagen para que salga el pixel pintado
     ctx.putImageData(imgData, 0, 0);
 
 
 };
+
+// funcion retrollamada del deslizador azul
 deslizador_B.oninput = () => {
 
+    rango_B.innerHTML = deslizador_B.value;
     //situamos la imagen original en el canvas
     ctx.drawImage(imgen, 0,0);
 
@@ -94,17 +114,21 @@ deslizador_B.oninput = () => {
     let data = imgData.data
 
     // obtenemos umbral del deslizador
-    umbral = deslizador_B.value;
+    let umbral_R = deslizador_R.value;
+    let umbral_G = deslizador_G.value;
+    let umbral_B = deslizador_B.value;
 
     // ahora borramos la componente roja de la imagen
     for(let i= 0; i<data.length; i+=4){
-        if (data[i] > umbral)
-         data[i+2] = umbral;
-      
-        // si queremos quitar la roja y la verde y quedarnos solo con azul
-        // activamos ambas data[i] y data[i+1] 
-        // si queremos quitar la verde es data[i+1]
-        // si queremos quitar la azul es data[i+2]
+        if (data[i] > umbral_R){
+            data[i] = umbral_R;
+        }
+        if (data[i+1] > umbral_G){
+            data[i+1] = umbral_G;
+        }
+        if (data[i+2] > umbral_B){
+            data[i+2] = umbral_B;
+        }    
     }
 
     // actualizamos la imagen para que salga el pixel pintado
