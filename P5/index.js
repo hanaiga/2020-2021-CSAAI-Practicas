1bde2bf8fc9f1b497d5a8657454b33e3d14924cd
hanaig <h.igalla.2018@alumnos.urjc.es>
2021-05-29 18:12:43 +0200

    empezando practica 5

---
+++ P5/index.js
@@ -0,0 +1,21 @@
+
+// Obtenemos los elementos del video
+let directo = document.getElementById("directo");
+const video1 = document.getElementById("video1");
+const video2 = document.getElementById("video2");
+const btn_video1 = document.getElementById("btn_video1");
+const btn_video2 = document.getElementById("btn_video2");
+const btn_test = document.getElementById("btn_test");
+const btn_on = document.getElementById("btn_src_on");
+const btn_off = document.getElementById("btn_src_off");
+
+// establecemos el tamaño o dimension de los videos
+directo.width = 420;
+directo.height = 220;
+video1.width = 200;
+video1.height = 100;
+video2.width = 200;
+video2.height = 100;
+
+// obtenemos la imagen test usada
+const test_imagen = "test.jpg";
